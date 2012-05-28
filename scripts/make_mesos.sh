59cc286c24ffd925c573595b4c5c8069db273adc
Rohit <rohit@tingendab.com>
2012-05-29 01:37:41 +0530

    Adding scripts to build hadoop for mesos.

--- scripts/make_mesos.sh
+++ scripts/make_mesos.sh
@@ -1,32 +1,20 @@
 #!/bin/bash
-if [ -z "$BLUEBIRD" ];
-then
-	export  BLUEBIRD="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
-fi
-echo BLUEBIRD directory is set to $BLUEBIRD
-
-if [ ! -d "$BLUEBIRD/dist/"];
-then
-	mkdir "$BLUEBIRD/dist/"
-fi
-
-export MESOS_HOME=$BLUEBIRD/dist/mesos
-
-if [ ! -d $MESOS_HOME];
-then
-	mkdir -p $MESOS_HOME
-fi
-
+. $( dirname "${BASH_SOURCE[0]}")/setenv.sh
 mkdir $BLUEBIRD/vendor/mesos/build
 cd $BLUEBIRD/vendor/mesos/
+
 echo Bootstrapping Mesos dev environment . . .
 ./bootstrap
 cd build
+
 echo Configuring Mesos . . .
 ../configure --prefix=$MESOS_HOME
+
 echo Building Mesos . . .
 make
+
 echo Installing Mesos . . .
 make install
-cd $BLUEBIRD
 
+cd $BLUEBIRD
+echo Mesos is setup
